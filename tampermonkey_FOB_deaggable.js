24becd81f11be87fdbde850876e459e29da328b9
Martin Loeffler <ml.loeffler@gmx.de>
2023-08-23 09:44:06 +0200

    work on tempermonkey, drag not working

--- tampermonkey_FOB_deaggable.js
+++ tampermonkey_FOB_deaggable.js
@@ -18,7 +18,58 @@
     const supportLauncher = document.querySelector("iframe#launcher-frame");
     if (supportLauncher) {
       clearInterval(interval);
-      alert("supportLauncher found");
+      addElements();
     }
   }, 1000);
 })();
+
+let offsetX;
+let offsetY;
+
+onDragStart = function (ev) {
+  const rect = ev.target.getBoundingClientRect();
+
+  offsetX = ev.clientX - rect.x;
+  offsetY = ev.clientY - rect.y;
+  //make element with the id "dropZone" visible
+  document.getElementById("dropZone").style.visibility = "visible";
+};
+
+drop_handler = function (ev) {
+  ev.preventDefault();
+
+  const left = parseInt(id2.style.left);
+  const top = parseInt(id2.style.top);
+
+  id1.style.left = ev.clientX - left - offsetX + "px";
+  id1.style.top = ev.clientY - top - offsetY + "px";
+  id2.appendChild(document.getElementById("id1"));
+  document.getElementById("id2").style.visibility = "hidden";
+};
+
+dragover_handler = function (ev) {
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "move";
+};
+
+function addElements() {
+  // add Attributes to the FOB to make it draggable and visible after drop
+  const supportLauncher = document.querySelector("iframe#launcher-frame");
+  supportLauncher.setAttribute("draggable", "true");
+  supportLauncher.setAttribute("ondragstart", "onDragStart(event)");
+
+  // add a drop zone for the FOB to the DOM
+  const dropZone = document.createElement("div");
+  dropZone.setAttribute("id", "dropZone");
+  dropZone.setAttribute("ondrop", "drop_handler(event)");
+  dropZone.setAttribute("ondragover", "dragover_handler(event)");
+  dropZone.style.width = "156px";
+  dropZone.style.height = "56px";
+  dropZone.style.position = "fixed";
+  dropZone.style.bottom = "22px";
+  dropZone.style.left = "22px";
+  dropZone.style.border = "1px solid #64be3c";
+  dropZone.style.cursor = "pointer";
+  dropZone.style.visibility = "hidden";
+  document.body.appendChild(dropZone);
+}
